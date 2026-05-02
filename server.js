7af7f97cee7b2ba299e111c21186214e2010c3f5
Kevin Payravi <kevinpayravi@gmail.com>
2026-05-02 13:28:47 +0200

    Upgrade to Node 24
    Can also remove node-fetch

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 /* eslint-env node, es6 */
 
 const express = require('express');
-const fetch = require('node-fetch');
-
 const cors = require('cors')
 const app = express();
 const port = parseInt(process.env.PORT, 10) || 3000;
